de682f2be0329ea1b7feb4d57118c172dc9dadae
revankale <rpkale.agpit@gmail.com>
2024-05-22 22:57:32 +0530

    create comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CreatePropertyComponent } from './pages/create-property/create-property.component';
+import { PropertyListComponent } from './pages/property-list/property-list.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreatePropertyComponent,
+    PropertyListComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
